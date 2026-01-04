a761a08bfa7a14e79249f0b705a0bfa8f7a44150
0lenaIva <88824086+0lenaIva@users.noreply.github.com>
2026-01-04 13:45:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import json
+import time
+import os
 
 pygame.init()
 
@@ -11,6 +13,17 @@ difficulty = ['легкий','середній','важкий']
 filename = 'settings.json'
 current_index = 0
 main_font = pygame.font.SysFont('Times New Roman', 36)
+main_font0 = pygame.font.SysFont('Times New Roman', 18)
+try:
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            if data.get("difficulty") in difficulty:
+                current_index = difficulty.index(data["difficulty"])
+except (FileNotFoundError, json.JSONDecodeError):
+        current_index = 0
+        with open(filename, 'w') as f:
+            json.dump({"difficulty":difficulty[0]}, f)
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
@@ -27,6 +40,8 @@ class Button:
         self.text_surf = self.font.render(self.text, True, self.text_color)
         self.text_rect = self.text_surf.get_rect(center=self.rect.center)
         screen.blit(self.text_surf, self.text_rect)
+    def is_clicked(self, event):
+        return event.type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos)
 
 play = Button(200, 80, 200, 50,COLOR, 'ГРАТИ')
 settings = Button(200, 160, 200, 50,COLOR, 'СКЛАДНІСТЬ')
@@ -35,15 +50,28 @@ exit = Button(200, 240, 200, 50,COLOR, 'ВИЙТИ')
 running = True
 
 while running:
+    screen.fill((0,0,0))
     diff_text = main_font.render('Складність: ' + difficulty[current_index], True, (200,200,200))
     screen.blit(diff_text, (SIZE[0] // 2 - diff_text.get_width()//2 , 20))
-    
+
     play.draw()
     settings.draw()
     exit.draw()
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
+        if exit.is_clicked(e):
+            running = False
+        if play.is_clicked(e):
+            diff_text = main_font0.render('Гра починається зі складністю: ' + difficulty[current_index], True, (200,200,200))
+            screen.blit(diff_text, (SIZE[0] // 2 - diff_text.get_width()//2 , 350))
+            pygame.display.update()
+            time.sleep(2)
+            running = False
+        if settings.is_clicked(e):
+            current_index = (current_index + 1) % len(difficulty)
+            with open(filename, 'w') as f:
+                json.dump({"difficulty":difficulty[current_index]}, f)
     pygame.display.update()
     clock.tick(FPS)
 quit()
